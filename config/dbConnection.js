829da51a82e95e9fb5c79716322be9824b0e250b
Kushal3009 <kushalpadhiyar030@gmail.com>
2025-06-16 23:20:42 +0530

    modify db connection file

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -10,7 +10,7 @@ export const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USERNAME,
     process.env.DB_PASSWORD, {
-    host: 'db',
+    host: 'localhost',
     dialect: 'mssql',
     logging: false,
     port: 1433
